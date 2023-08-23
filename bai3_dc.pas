763341a7cd16742894ae090e60fd06e4c9d28d3c
duyhieu12890 <duyhieu12890@gmail.com>
2023-08-23 11:47:28 +0700

    Pascal Coding

--- bai3_dc.pas
+++ bai3_dc.pas
@@ -1,4 +1,13 @@
 uses crt;
-var 
+var s:string;
+A:array[1..50] of byte;  //bien cua day so trong xau
+B:array[1..10] of byte;  //bien kiem tra so do bi trung lap
 begin
+    write('nhap day so bat ki khong qua 50 chu so:');readln(s);
+    tmp:=length(s);
+    for i:=1 to tmp do A[i]:=s[i];
+    for i:=1 to tmp do
+    begin
+        if A[i] = 
+    end;
 end.
